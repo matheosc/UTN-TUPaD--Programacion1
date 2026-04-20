9e5c7bbb03b3c0e864cddac28f350d7f3f51c6c3
Matheo Santiago Cardozo <89360062+matheosc@users.noreply.github.com>
2026-04-20 16:01:18 -0300

    Finalizado Ejercicio 8 de Unidad 6

--- 06 Funciones/EJ8.py
+++ 06 Funciones/EJ8.py
@@ -5,14 +5,14 @@ alt_usuario = "a"
 def calcular_imc(peso, altura):
     return peso / (altura**2)
 
-peso_usuario = input("Ingrese su peso en kilogramos: ")
+peso_usuario = input("Ingrese su peso en gramos: ")
 while not peso_usuario.isdigit():
-    peso_usuario = input("Error, ingrese su peso en kilogramos: ")
-peso_usuario = float(peso_usuario)
+    peso_usuario = input("Error, ingrese su peso en gramos: ")
+peso_usuario = float(peso_usuario)/1000
 
-alt_usuario = input("Ingrese su altura en metros: ")
-while not isinstance(alt_usuario, float):
-    alt_usuario= input("Error, ingrese su altura en metros: ")
-alt_usuario = float(alt_usuario)
+alt_usuario = input("Ingrese su altura en centímetros: ")
+while not alt_usuario.isdigit():
+    alt_usuario = input("Error, ingrese su altura en centímetros: ")
+alt_usuario = float(alt_usuario)/100
 
 print(f"Su IMC es de: {calcular_imc(peso_usuario, alt_usuario):.2f}")
