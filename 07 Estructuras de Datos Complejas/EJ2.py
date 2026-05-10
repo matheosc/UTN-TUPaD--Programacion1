5b8d81e0cfc7f68ce2ba62b2a091796a1941495e
Matheo Cardozo <matheo.santiagoc@gmail.com>
2026-05-10 04:51:33 -0300

    Agregado Ejercicio 2 de Unidad 7

---
+++ 07 Estructuras de Datos Complejas/EJ2.py
@@ -0,0 +1,12 @@
+# EJERCICIO 2
+
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
+1450, 'Naranja': 1200, 'Manzana': 1500, 'Pera': 2300}
+
+print(precios_frutas)
+
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melón'] = 2800
+
+print(precios_frutas)
